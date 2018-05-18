f340439d542b9acf33f8dbe83dbe14b759eb6b3e
poly-media <andi@poly.media>
2018-05-18 15:55:25 +0200

    navigation

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Home from './screens/Home';
+import RootNavigator from './navigation/RootNavigator';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Home />
-      </View>
+        <RootNavigator />
     );
   }
 }
